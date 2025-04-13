5728205fadf4f988784c4e27e602f870ad4730a1
Arunav Singh <arunavsingh999@gmail.com>
2025-04-13 18:49:01 +0530

    added logic for nex and prev buttons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "./Card.jsx";
 
 import USERS_DATA from "../util/data.js";
 
 export default function Cards() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const currentUser = USERS_DATA[currentIndex];
+
+  function prevButton() {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + USERS_DATA.length) % USERS_DATA.length
+    );
+  }
+
+  function nextButton() {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % USERS_DATA.length);
+  }
+
   return (
     <>
       <div className="cards">
-        {USERS_DATA.map((user) => (
+        <button onClick={prevButton}>Previous</button>
+        {/* {USERS_DATA.map((user) => (
           <Card
             key={user.id}
             img={user.image}
@@ -16,7 +31,15 @@ export default function Cards() {
             email={user.email}
             bio={user.bio}
           />
-        ))}
+        ))} */}
+        <Card
+          key={currentUser.id}
+          img={currentUser.image}
+          name={currentUser.name}
+          email={currentUser.email}
+          bio={currentUser.bio}
+        />
+        <button onClick={nextButton}>Next</button>
       </div>
     </>
   );
