5e700c7df29e2dae76d043f4789c37b36aead1b0
Arunav Singh <arunavsingh999@gmail.com>
2025-04-13 15:46:49 +0530

    data and formatter added

---
+++ src/util/data.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 1,
+    name: "Arunav Singh",
+    email: "arunav@example.com",
+    bio: "Frontend developer who loves React and SCSS!",
+    image: "/assets/dummy.jpg"
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "jane@example.com",
+    bio: "Creative designer and CSS wizard.",
+    image: "/assets/dummy.jpg"
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    email: "john@example.com",
+    bio: "JavaScript ninja and web performance geek.",
+    image: "/assets/dummy.jpg"
+  }
+];
+
+export default users;
