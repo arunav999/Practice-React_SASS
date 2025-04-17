502eeeb236a4c633335eca8b8155f271bb037d38
Arunav Singh <arunavsingh999@gmail.com>
2025-04-17 15:53:31 +0530

    modified folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Header from "./components/Header.jsx";
-import Cards from "./components/Cards.jsx";
+import Header from "./Cards_React/components/Header.jsx";
+import Cards from "./Cards_React/components/Cards.jsx";
 
 export default function App() {
   return (
