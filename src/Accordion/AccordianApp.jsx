f71f6a429730a3f7dd24c3bb587ca060fa27af4e
Arunav Singh <arunavsingh999@gmail.com>
2025-04-17 22:43:08 +0530

    added scss for accordian comp

--- src/Accordion/AccordianApp.jsx
+++ src/Accordion/AccordianApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function AccordianApp() {
   return (
     <>
-      
+      <p className="name">Arunav</p>
     </>
-  )
+  );
 }
