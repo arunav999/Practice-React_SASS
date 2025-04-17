c45fe6b83126b56ff987e870aaa9681d8885b301
Arunav Singh <arunavsingh999@gmail.com>
2025-04-17 16:22:28 +0530

    half-ref and modal

--- src/Ref_and_Portal/Ref/RefInput.jsx
+++ src/Ref_and_Portal/Ref/RefInput.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function RefInput() {
+  const inputRef = useRef();
+
+  function handleClick() {
+    const focus = inputRef.current.focus;
+  }
+
   return (
-    <div>
-      <p className="Aru">Arunav</p>
-    </div>
+    <>
+      <div className="refInput">
+        <input type="text" ref={inputRef} />
+        <button onClick={handleClick}>Click</button>
+      </div>
+    </>
   );
 }
