2368092ec64d11edb2327ba7c7210b13d2b9e1d2
Arunav Singh <arunavsingh999@gmail.com>
2025-04-22 20:17:05 +0530

    image slider completed

--- src/ImageSlider/ImageSliderApp.jsx
+++ src/ImageSlider/ImageSliderApp.jsx
@@ -4,7 +4,11 @@ import ImageSlider from "./components/imageSlider.jsx";
 export default function ImageSliderApp() {
   return (
     <>
-      <ImageSlider />
+      <ImageSlider
+        url={"http://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
